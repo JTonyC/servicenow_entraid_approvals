cc42b90a8cfe3aac26884bcaa428cced6ea6781e
JTonyC <tony_chaffe@hotmail.com>
2026-03-22 14:31:58 +0000

    SCRUM-24 Testing amendments

--- tests/performance/smoke_test.js
+++ tests/performance/smoke_test.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
-import { jUnit } from 'https://jslib.k6.io/k6-reporter/0.0.1/index.js';
+import { jUnit } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
 export let options = {
   vus: 10,
